1e5d35eab51637b067c3611efa4bf70cea5e16c2
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-04-14 13:59:13 -0700

    Add assert statement on input arg

--- Functions/rECI2RTN.m
+++ Functions/rECI2RTN.m
@@ -1,6 +1,8 @@
 function R_ECI2RTN = rECI2RTN(vec_ECI)
     %ECI2RTN convert a vector in ECI to RTN frame
     % vec_ECI should be [r; v]_ECI
+    assert(all(size(vec_ECI) == [6, 1]), "Vector provided to rECI2RTN() must be size 6x1")
+    
     r = vec_ECI(1:3);
     v = vec_ECI(4:6);
     n = cross(r, v);
@@ -9,7 +11,6 @@ function R_ECI2RTN = rECI2RTN(vec_ECI)
     N = r / norm(n);
     T = cross(N, R);
 
-    %R_eci2rtn = hcat(R, T, N)'
     R_ECI2RTN = [R, T, N]';
 end
 

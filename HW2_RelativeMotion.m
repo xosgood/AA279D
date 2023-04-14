b4ffdba4cb4d5cf31f2cfea2eb9001fe00b4a873
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-04-14 11:21:49 -0700

    Create main HW2 script

---
+++ HW2_RelativeMotion.m
@@ -0,0 +1,34 @@
+% Paxton Scott and Chris Osgood
+% 4/14/23
+% AA279D HW1
+clc; clear; close all;
+addpath("Functions/")
+
+%% a) initial orbits
+% chief
+a_0 = 7000; % km
+e_0 = 0.001;
+i_0 = deg2rad(98);
+RAAN_0 = 0;
+omega_0 = deg2rad(90);
+nu_0 = 0;
+% oe_0 = [a; e; i; RAAN; omega; nu];
+
+% deputy
+a_1 = a_0;
+e_1 = 0.001;
+i_1 = deg2rad(98);
+RAAN_1 = 0;
+omega_1 = deg2rad(90);
+nu_1 = 0;
+% oe_0 = [a; e; i; RAAN; omega; nu];
+
+%% b) relative orbit sim
+
+
+%% c) absolute orbit sim of chief and deputy
+
+
+%% d) error between relative sim and absolute sim
+
+

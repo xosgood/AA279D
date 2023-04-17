80a909e0dd038dfe80acfe74da71f64fe066252d
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-04-17 12:16:42 -0700

    Work on HW2 part d

--- HW2_RelativeMotion.m
+++ HW2_RelativeMotion.m
@@ -38,8 +38,8 @@ tspan = linspace(0, t_f, n_iter); % [0, t_f];
 r0 = norm(r_ECI_0);
 v0 = norm(v_ECI_0);
 
-theta = nu_0 - omega_0;
-theta_dot = v0/r0;
+theta = nu_0 + omega_0;
+theta_dot = norm(cross(r_ECI_0, v_ECI_0))/r0^2; % v0 / r0
 state0 = [r_RTN_abssim; theta; r0; v_RTN_abssim; theta_dot; v0];
 
 options = odeset('RelTol', 1e-6, 'AbsTol', 1e-9);
@@ -97,15 +97,93 @@ figure;
 sgtitle("Non-Dimensionalized Relative Orbit (RTN) from Absolute Orbit Simulation")
 PlotRTN_NonDim(t, r_RTN_abssim, v_RTN_abssim, a_0);
 % plot RTN in 3D
-figure;
-title("Relative Motion in RTN from Absolute Orbit Simulation");
-Plot3RTN(r_RTN_abssim, 0);
-figure;
-title("Non-Dimensionalized Relative Motion in RTN from Absolute Orbit Simulation");
-Plot3RTN(r_RTN_abssim, a_0);
-
-
+% figure;
+% Plot3RTN(r_RTN_abssim, 0);
+% title("Relative Motion in RTN from Absolute Orbit Simulation");
+% figure;
+% Plot3RTN(r_RTN_abssim, a_0);
+% title("Non-Dimensionalized Relative Motion in RTN from Absolute Orbit Simulation");
 
 %% d) error between relative sim and absolute sim
+r_RTN_err = abs(r_RTN_relsim - r_RTN_abssim);
+v_RTN_err = abs(v_RTN_relsim - v_RTN_abssim);
+figure;
+PlotRTN(t, r_RTN_err, v_RTN_err);
+sgtitle("RTN Error Between Relative Motion and Absolute Orbit Simulations");
 
+% Repeat parts b-c with a difference in semi-major axis
+% deputy
+a_1 = a_0 + 100;
 
+% repeating part c
+[r_ECI_0, v_ECI_0] = OE2ECI(a_0, e_0, i_0, RAAN_0, omega_0, nu_0);
+[r_ECI_1, v_ECI_1] = OE2ECI(a_1, e_1, i_1, RAAN_1, omega_1, nu_1);
+
+[r_RTN_abssim, v_RTN_abssim] = ECI2RTN(r_ECI_0, v_ECI_0, r_ECI_1, v_ECI_1);
+
+r0 = norm(r_ECI_0);
+v0 = norm(v_ECI_0);
+
+theta = nu_0 + omega_0;
+theta_dot = norm(cross(r_ECI_0, v_ECI_0))/r0^2;
+state0 = [r_RTN_abssim; theta; r0; v_RTN_abssim; theta_dot; v0];
+
+options = odeset('RelTol', 1e-6, 'AbsTol', 1e-9);
+[t, state_out] = ode113(@RelativeMotionDifEqRTN, tspan, state0, options);
+
+% pull out RTN position and velocity
+r_RTN_relsim = state_out(:,1:3)';
+v_RTN_relsim = state_out(:,6:8)';
+
+% plot RTN orbit
+figure;
+sgtitle("Relative Orbit (RTN) from Relative Motion Simulation, with da")
+PlotRTN(t, r_RTN_relsim, v_RTN_relsim);
+% plot RTN orbit, non-dimensionalized by semi-major axis
+figure;
+sgtitle("Non-Dimensionalized Relative Orbit (RTN) from Relative Motion Simulation, with da")
+PlotRTN_NonDim(t, r_RTN_relsim, v_RTN_relsim, a_0);
+
+% repeating part c
+abs_data_0 = SimulateOrbitFromOE(a_0, e_0, i_0, RAAN_0, omega_0, M_0, geod_station, t_0_MJD, t_f_MJD, n_iter);
+abs_data_1 = SimulateOrbitFromOE(a_1, e_1, i_1, RAAN_1, omega_1, M_1, geod_station, t_0_MJD, t_f_MJD, n_iter);
+
+r_ECI_0 = abs_data_0.r_ECI_vec;
+v_ECI_0 = abs_data_0.v_ECI_vec;
+r_ECI_1 = abs_data_1.r_ECI_vec;
+v_ECI_1 = abs_data_1.v_ECI_vec;
+
+% turn ECI to RTN for deputy state
+[r_RTN_abssim, v_RTN_abssim] = ECI2RTN_Vectorized(r_ECI_0, v_ECI_0, r_ECI_1, v_ECI_1);
+
+% plot orbits in ECI
+figure;
+PlotEarth();
+plot3(r_ECI_0(1,:), r_ECI_0(2,:), r_ECI_0(3,:), 'r.');
+plot3(r_ECI_1(1,:), r_ECI_1(2,:), r_ECI_1(3,:), 'g.');
+title("Absolute ECI Orbits");
+legend("Chief", "Deputy");
+xlabel('I (km)'); ylabel('J (km)'); zlabel('K (km)');
+
+% plot RTN orbit
+figure;
+sgtitle("Relative Orbit (RTN) from Absolute Orbit Simulation, with da")
+PlotRTN(t, r_RTN_abssim, v_RTN_abssim);
+% plot RTN orbit, non-dimensionalized by semi-major axis
+figure;
+sgtitle("Non-Dimensionalized Relative Orbit (RTN) from Absolute Orbit Simulation, with da")
+PlotRTN_NonDim(t, r_RTN_abssim, v_RTN_abssim, a_0);
+% plot RTN in 3D
+% figure;
+% Plot3RTN(r_RTN_abssim, 0);
+% title("Relative Motion in RTN from Absolute Orbit Simulation, with da");
+% figure;
+% Plot3RTN(r_RTN_abssim, a_0);
+% title("Non-Dimensionalized Relative Motion in RTN from Absolute Orbit Simulation, with da");
+
+% plot errors for this new sim with da
+r_RTN_err = abs(r_RTN_relsim - r_RTN_abssim);
+v_RTN_err = abs(v_RTN_relsim - v_RTN_abssim);
+figure;
+PlotRTN(t, r_RTN_err, v_RTN_err);
+sgtitle("RTN Error Between Relative Motion and Absolute Orbit Simulations with da");

6a9f09d598ccee6cbded2c792c2afccad33fc243
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-04-17 20:13:53 -0700

    HW2 part e: add maneuver

--- HW2_RelativeMotion.m
+++ HW2_RelativeMotion.m
@@ -33,17 +33,17 @@ tspan = linspace(0, t_f, n_iter); % [0, t_f];
 [r_ECI_0, v_ECI_0] = OE2ECI(a_0, e_0, i_0, RAAN_0, omega_0, nu_0);
 [r_ECI_1, v_ECI_1] = OE2ECI(a_1, e_1, i_1, RAAN_1, omega_1, nu_1);
 
-[r_RTN_abssim, v_RTN_abssim] = ECI2RTN(r_ECI_0, v_ECI_0, r_ECI_1, v_ECI_1);
+[r_RTN, v_RTN] = ECI2RTN(r_ECI_0, v_ECI_0, r_ECI_1, v_ECI_1);
 
 r0 = norm(r_ECI_0);
 v0 = norm(v_ECI_0);
 
 theta = nu_0 + omega_0;
 theta_dot = norm(cross(r_ECI_0, v_ECI_0))/r0^2; % v0 / r0
-state0 = [r_RTN_abssim; theta; r0; v_RTN_abssim; theta_dot; v0];
+state0 = [r_RTN; theta; r0; v_RTN; theta_dot; v0];
 
 options = odeset('RelTol', 1e-6, 'AbsTol', 1e-9);
-[t, state_out] = ode113(@RelativeMotionDifEqRTN, tspan, state0, options);
+[t, state_out] = ode45(@RelativeMotionDifEqRTN, tspan, state0, options);
 
 % pull out RTN position and velocity
 r_RTN_relsim = state_out(:,1:3)';
@@ -113,7 +113,8 @@ sgtitle("RTN Error Between Relative Motion and Absolute Orbit Simulations");
 
 % Repeat parts b-c with a difference in semi-major axis
 % deputy
-a_1 = a_0 + 100;
+da = 100;
+a_1 = a_0 + da;
 
 % repeating part c
 [r_ECI_0, v_ECI_0] = OE2ECI(a_0, e_0, i_0, RAAN_0, omega_0, nu_0);
@@ -187,3 +188,44 @@ v_RTN_err = abs(v_RTN_relsim - v_RTN_abssim);
 figure;
 PlotRTN(t, r_RTN_err, v_RTN_err);
 sgtitle("RTN Error Between Relative Motion and Absolute Orbit Simulations with da");
+
+
+%% e+f) apply manuever
+
+% use T of deputy
+T = 2 * pi * sqrt(a_1^3 / mu);
+
+n_orbits_pre_dv = 2 - nu_1/(2*pi); % after 2 orbits (minus the initial true anomaly), it is back at periapsis
+n_iter = 30 * n_orbits_pre_dv;
+t_f = n_orbits_pre_dv * T;
+tspan = linspace(0, t_f, n_iter); % [0, t_f];
+
+[t_pre_dv, state_out_pre_dv] = ode113(@RelativeMotionDifEqRTN, tspan, state0, options);
+
+n_orbits_post_dv = 5;
+n_iter = 30 * (n_orbits_post_dv - n_orbits_pre_dv);
+t_f_2 = n_orbits_post_dv * T;
+tspan = linspace(t_f, t_f_2, n_iter); % [0, t_f];
+
+% compute dv
+n = sqrt(mu / a_1^3);
+r = norm([state_out_pre_dv(end,1) + state_out_pre_dv(end,5), state_out_pre_dv(end,2:3)]);
+dv_T = -da * n * r / (2 * a_1 * sqrt(1 - e_1^2));
+
+% apply dv
+state0 = state_out_pre_dv(end,:);
+state0(7) = state0(7) + dv_T;
+[t_post_dv, state_out_post_dv] = ode113(@RelativeMotionDifEqRTN, tspan, state0, options);
+
+r_RTN_pre_dv = state_out_pre_dv(:,1:3)';
+v_RTN_pre_dv = state_out_pre_dv(:,6:8)';
+r_RTN_post_dv = state_out_post_dv(:,1:3)';
+v_RTN_post_dv = state_out_post_dv(:,6:8)';
+
+t_tot_maneuver = [t_pre_dv; t_post_dv];
+r_RTN_tot_maneuver = [r_RTN_pre_dv, r_RTN_post_dv];
+v_RTN_tot_maneuver = [v_RTN_pre_dv, v_RTN_post_dv];
+
+figure;
+PlotRTN(t_tot_maneuver, r_RTN_tot_maneuver, v_RTN_tot_maneuver);
+sgtitle("Relative Motion with Along-Track Maneuver to Cancel da");

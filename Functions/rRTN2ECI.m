d79b8efd39080f87e7c30bef76a8a6d2ba315d8c
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-04-18 10:53:04 -0700

    Add rotation function RTN2ECI

---
+++ Functions/rRTN2ECI.m
@@ -0,0 +1,16 @@
+function R_RTN2ECI = rRTN2ECI(vec_ECI)
+    %rECI2RTN returns a rotation matrix from ECI to RTN frame
+    % vec_ECI should be [r; v]_ECI
+    assert(all(size(vec_ECI) == [6, 1]), "Vector provided to rECI2RTN() must be size 6x1")
+    
+    r = vec_ECI(1:3);
+    v = vec_ECI(4:6);
+    n = cross(r, v);
+
+    R = r / norm(r);
+    N = n / norm(n);
+    T = cross(N, R);
+
+    R_RTN2ECI = [R, T, N];
+end
+

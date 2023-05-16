bde60c4958fd8671ff2c8a4061adbfa952fbe815
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-05-16 11:57:21 -0700

    Fix typo

--- Functions/Controls/ContinousControl/B_Control_ReducedModel.m
+++ Functions/Controls/ContinousControl/B_Control_ReducedModel.m
@@ -7,27 +7,27 @@ function B = B_Control_ReducedModel(oe_c)
     a = oe_c(1);
     e = oe_c(2);
     i = oe_c(3);
-    omega = oe_c(5);
+    w = oe_c(5);
     nu = oe_c(6);
     
     n = sqrt(mu / a^3);
     eta = sqrt(1 - e^2);
     
-    e_x = e*cos(omega);
-    e_y = e*sin(omega);
+    e_x = e*cos(w);
+    e_y = e*sin(w);
     
     B = zeros(5, 2);
 
     B(1, 1) = 2/eta * (1 + e*cos(nu));
     
-    B(2, 1) = eta * ((2 + e*cos(nu)) * cos(omega + nu) + e_x) / (1 + e*cos(nu));
+    B(2, 1) = eta * ((2 + e*cos(nu)) * cos(w + nu) + e_x) / (1 + e*cos(nu));
     B(2, 2) = (eta * e_y)/ tan(i) * sin(w + nu) / (1 + e*cos(nu));
 
-    B(3, 1) = eta * ((2 + e*cos(nu)) * sin(omega+nu) + e) /(1 + e*cos(nu));
+    B(3, 1) = eta * ((2 + e*cos(nu)) * sin(w + nu) + e_y) /(1 + e*cos(nu));
     B(3, 2) = (-eta * e_x)/ tan(i) * sin(w + nu) / (1 + e*cos(nu));
 
-    B(4, 2) = eta * cos(omega + nu) / (1 + e*cos(nu));
-    B(5, 2) = eta * sin(omega + nu) / (1 + e*cos(nu));
+    B(4, 2) = eta * cos(w + nu) / (1 + e*cos(nu));
+    B(5, 2) = eta * sin(w + nu) / (1 + e*cos(nu));
 
     B = B/(a*n);
 end 

156c835b9d53d4b29968cd44fb8c3ab1aea2d54f
Paxton Scott <paxtonsc@stanford.edu>
2023-05-16 11:53:41 -0700

    removing unused variables, adding comments.

--- Functions/Controls/ContinousControl/A_Control_ReducedModel.m
+++ Functions/Controls/ContinousControl/A_Control_ReducedModel.m
@@ -1,6 +1,6 @@
 function A = A_Control_ReducedModel(oe_c)
-    %A_CONTROL_REDUCEDMODEL Summary of this function goes here
-    %   Detailed explanation goes here
+    % Returns the reduced plant matrix given the orbital elements of the
+    % chief. 
     
     mu = 3.986e5; % (km^3 / s^2) for earth
     R_E = 6378.1; % equatorial radius of earth in km
@@ -9,11 +9,8 @@ function A = A_Control_ReducedModel(oe_c)
     a = oe_c(1);
     e = oe_c(2);
     i = oe_c(3);
-    RAAN = oe_c(4);
     omega = oe_c(5);
-    nu = oe_c(6);
     
-    n = sqrt(mu / a^3);
     eta = sqrt(1 - e^2);
     kappa = 3 * J2 * R_E^2 * sqrt(mu) / (4 * a^3.5 * eta^4);
     
@@ -22,10 +19,6 @@ function A = A_Control_ReducedModel(oe_c)
     
     C = sin(omega);
     D = cos(omega);
-    E = 1 + eta;
-    F = 4 + 3*eta;
-    G = 1 / eta^2;  
-    P = 3*cos(i)^2 - 1;
     Q = 5*cos(i)^2 -1; 
     S = sin(2*i);
     T = sin(i)^2;
--- Functions/Controls/ContinousControl/B_Control_ReducedModel.m
+++ Functions/Controls/ContinousControl/B_Control_ReducedModel.m
@@ -1,33 +1,20 @@
 function B = B_Control_ReducedModel(oe_c)
-    %B_CONTROL_REDUCEDMODEL Summary of this function goes here
-    %   Detailed explanation goes here
+    % Returns the reduced control input matrix given 
+    % the orbital elements of the chief. 
+
     mu = 3.986e5; % (km^3 / s^2) for earth
-    R_E = 6378.1; % equatorial radius of earth in km
-    J2 = 0.108263e-2; % for earth
     
     a = oe_c(1);
     e = oe_c(2);
     i = oe_c(3);
-    RAAN = oe_c(4);
     omega = oe_c(5);
     nu = oe_c(6);
     
     n = sqrt(mu / a^3);
     eta = sqrt(1 - e^2);
-    kappa = 3 * J2 * R_E^2 * sqrt(mu) / (4 * a^3.5 * eta^4);
     
     e_x = e*cos(omega);
     e_y = e*sin(omega);
-    
-    C = sin(omega);
-    D = cos(omega);
-    E = 1 + eta;
-    F = 4 + 3*eta;
-    G = 1 / eta^2;  
-    P = 3*cos(i)^2 - 1;
-    Q = 5*cos(i)^2 -1; 
-    S = sin(2*i);
-    T = sin(i)^2;
     
     B = zeros(5, 2);
 

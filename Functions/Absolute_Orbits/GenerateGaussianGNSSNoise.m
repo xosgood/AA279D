618812a92f4205ce1f8ac08a7d637c85e0f5bda5
Paxton Scott <paxtonsc@stanford.edu>
2023-05-30 10:24:57 -0700

    deleting random gaussian noise function.

--- Functions/Absolute_Orbits/GenerateGaussianGNSSNoise.m
+++
@@ -1,7 +0,0 @@
-function [r_noise, v_noise] = GenerateGaussianGNSSNoise(r_noise_std, v_noise_std, n)
-    r_sigma = eye(3) * r_noise_std; 
-    v_sigma = eye(3) * v_noise_std; 
-
-    r_noise = sqrtm(r_sigma) * randn(3,n);
-    v_noise = sqrt(v_sigma) * randn(3,n);
-end

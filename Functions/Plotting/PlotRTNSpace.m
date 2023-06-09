b81e0f25bae890757b1dc718c863b0a77045ebf6
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-06-09 10:20:11 -0700

    Add grid on

--- Functions/Plotting/PlotRTNSpace.m
+++ Functions/Plotting/PlotRTNSpace.m
@@ -1,6 +1,6 @@
 function PlotRTNSpace(x_RTN)
     % Plot relative position in 3D. 
-    figure;
+    figure; grid on;
     plot3(x_RTN(:,1), x_RTN(:,2), x_RTN(:,3));
     xlabel("R [km]");
     ylabel("T [km]");
@@ -9,7 +9,7 @@ function PlotRTNSpace(x_RTN)
     axis equal;
     
     % Plot relative velocity in 3D. 
-    figure;
+    figure; grid on;
     plot3(x_RTN(:,4), x_RTN(:,5), x_RTN(:,6));
     xlabel("R [km/s]");
     ylabel("T [km/s]");
@@ -19,18 +19,18 @@ function PlotRTNSpace(x_RTN)
     
     % Plot relative position in TR, NR, TN plane.
     figure;
-    subplot(3,1,1);
+    subplot(3,1,1); grid on;
     plot(x_RTN(:,2), x_RTN(:,1));
     axis equal;
     title("Relative position in RTN over time");
     xlabel("T [km]");
     ylabel("R [km]");
-    subplot(3,1,2);
+    subplot(3,1,2); grid on;
     plot(x_RTN(:,3), x_RTN(:,1));
     axis equal;
     xlabel("N [km]");
     ylabel("R [km]");
-    subplot(3,1,3);
+    subplot(3,1,3); grid on;
     plot(x_RTN(:,2), x_RTN(:,3));
     axis equal;
     xlabel("T [km]");
@@ -38,18 +38,18 @@ function PlotRTNSpace(x_RTN)
     
     % Plot relative velocity in TR, NR, TN plane.
     figure;
-    subplot(3,1,1);
+    subplot(3,1,1); grid on;
     plot(x_RTN(:,5), x_RTN(:,4));
     axis equal;
     title("Relative velocity in RTN over time");
     xlabel("T [km/s]");
     ylabel("R [km/s]");
-    subplot(3,1,2);
+    subplot(3,1,2); grid on;
     plot(x_RTN(:,6), x_RTN(:,4));
     axis equal;
     xlabel("N [km/s]");
     ylabel("R [km/s]");
-    subplot(3,1,3);
+    subplot(3,1,3); grid on;
     plot(x_RTN(:,5), x_RTN(:,6));
     axis equal;
     xlabel("T [km/s]");
